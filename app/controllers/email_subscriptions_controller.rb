829cbfe706b9357f1d52e228916950df0b5d7906
juuuno-coder <juuuno1116@gmail.com>
2026-03-01 22:15:02 +0900

    이메일 구독 SMTP 에러 처리 개선
    - SMTP_PASSWORD 미설정 시에도 정상 동작하도록 수정
    - 이메일 발송 실패 시 로그만 기록하고 사용자에게는 성공 메시지 표시
    - DB 저장은 정상적으로 진행됨

--- app/controllers/email_subscriptions_controller.rb
+++ app/controllers/email_subscriptions_controller.rb
@@ -5,8 +5,17 @@ class EmailSubscriptionsController < ApplicationController
     @subscription = EmailSubscription.new(email: params[:email])
 
     if @subscription.save
-      # juuuno@naver.com으로 알림 발송 (즉시 전송)
-      SubscriptionMailer.new_subscriber(@subscription).deliver_now
+      # juuuno@naver.com으로 알림 발송 (SMTP 설정이 있을 때만)
+      if ENV["SMTP_PASSWORD"].present?
+        begin
+          SubscriptionMailer.new_subscriber(@subscription).deliver_now
+        rescue => e
+          Rails.logger.error "이메일 발송 실패: #{e.message}"
+          # 에러가 발생해도 사용자에게는 성공 메시지 표시 (DB 저장은 성공했으므로)
+        end
+      else
+        Rails.logger.warn "SMTP_PASSWORD가 설정되지 않아 이메일 발송을 건너뜁니다."
+      end
 
       redirect_to root_path, notice: "출시 알림 신청이 완료되었습니다! 🎉"
     else
